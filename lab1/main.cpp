329757ef829ae3cad94b50f96b092cd1f7de87f6
Jethro <iv.ilyushka2003@gmail.com>
2023-09-19 12:56:54 +0300

    a

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -6,87 +6,132 @@
 #include <GL/glu.h>
 #include "GL/freeglut.h"
 
-// ���������� ������������ ���� ����������� ����������
+#include "libs/glm/glm.hpp"
+#include "libs/glm/gtc/matrix_transform.hpp"
+#include "libs/glm/gtc/type_ptr.hpp" 
+using namespace glm;
+
+#include <vector>
+
+
+
+// используем пространство имен стандартной библиотеки
 using namespace std;
 
-// �������, ���������� ��� ��������� �������� ����
+const int ColorNumber = 5;
+vector<vec3> Colors;
+{ (0, 0, 0), (1, 1, 1), (0, 0, 1), (1, 0, 0), (1, 0, 1) };
+int colorID = 0;	
+bool autoChange = false;
+
+// функция, вызываемая при изменении размеров окна
 void reshape(int w, int h)
 {
-	// ���������� ����� ������� ���������, ������ ���� ������� ����
+	// установить новую область просмотра, равную всей области окна
 	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
 
-	// ���������� ������� �������� � ���������� ��������
+	// установить матрицу проекции с правильным аспектом
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluPerspective(25.0, (float)w / h, 0.2, 70.0);
 };
 
-// ������� ���������� ��� ����������� ����
-// � ��� ����� � �������������, �� �������� glutPostRedisplay
+// функция вызывается при перерисовке окна
+// в том числе и принудительно, по командам glutPostRedisplay
 void display(void)
 {
-	// �������� ����� ����� � ����� �������
+	// отчищаем буфер цвета и буфер глубины
 	glClearColor(0.22, 0.88, 0.11, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	// �������� ���� �������
+	// включаем тест глубины
 	glEnable(GL_DEPTH_TEST);
 
-	// ������������� ������
+	// устанавливаем камеру
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	gluLookAt(5, 5, 7.5, 0, 0, 0, 0, 1, 0);
 
-	// ������� ������ - ������� (1,0,0) ������
-	glColor3f(1.0, 0.0, 0.0);
+	// выводим объект - чайник
+	static int framesCount = 0;
+	if (autoChange == true and (framesCount == 0)) {
+		printf("autocolor\n");
+		colorID += 1;
+		colorID = colorID % ColorNumber;
+	}
+	glColor3f(Colors[colorID][0], Colors[colorID][1], Colors[colorID][2]);
 	glutWireTeapot(1.0);
 
-	// ����� ��������� � ������� �������
+	// смена переднего и заднего буферов
 	glutSwapBuffers();
+
+	framesCount += 1;
+	framesCount = framesCount % 50;
 };
 
-// ������� ���������� ������ 20 ��
+// функция вызывается каждые 20 мс
 void simulation(int value)
 {
-	// ������������� ������� ����, ��� ���� ��������� � �����������
+	// устанавливаем признак того, что окно нуждается в перерисовке
 	glutPostRedisplay();
-	// ��� �� ������� ����� ������� ��� ��� ����� 20 ��
+	// эта же функция будет вызвана еще раз через 20 мс
 	glutTimerFunc(20, simulation, 0);
 };
 
-// ������� ��������� ������� ������
+void colorSwitch() {
+
+}
+
+// Функция обработки нажатия клавиш
 void keyboardFunc(unsigned char key, int x, int y)
 {
 	printf("Key code is %i\n", key);
+	switch (key)
+	{
+	case 32:{
+		colorID += 1;
+		colorID = colorID % ColorNumber;
+
+		printf("manual change\n");
+		break;
+	}
+	case 49: {
+		autoChange = !autoChange;
+		printf("change mode\n");
+		break;
+	}
+	default:
+		break;
+	}
 };
 
 void main(int argc, char **argv)
 {
 	setlocale(LC_ALL, "ru");
 
-	// ������������� ���������� GLUT
+	// инициализация библиотеки GLUT
 	glutInit(&argc, argv);
-	// ������������� ������� (������ ������)
+	// инициализация дисплея (формат вывода)
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH | GLUT_MULTISAMPLE);
 
-	// �������� ����:
-	// 1. ������������� ������� ����� ���� ����
+	// СОЗДАНИЕ ОКНА:
+	// 1. устанавливаем верхний левый угол окна
 	glutInitWindowPosition(200, 200);
-	// 2. ������������� ������ ����
+	// 2. устанавливаем размер окна
 	glutInitWindowSize(800, 600);
-	// 3. ������� ����
+	// 3. создаем окно
 	glutCreateWindow("Laba_01");
 
-	// ��������� ������� ��������� ������
-	// ������������� �������, ������� ����� ���������� ��� ����������� ����
+	// УСТАНОВКА ФУНКЦИЙ ОБРАТНОГО ВЫЗОВА
+	// устанавливаем функцию, которая будет вызываться для перерисовки окна
 	glutDisplayFunc(display);
-	// ������������� �������, ������� ����� ���������� ��� ��������� �������� ����
+	// устанавливаем функцию, которая будет вызываться при изменении размеров окна
 	glutReshapeFunc(reshape);
-	// ������������� �������, ������� ����� ������� ����� 20 ��
+	// устанавливаем функцию, которая будет вызвана через 20 мс
 	glutTimerFunc(20, simulation, 0);
-	// ������������� �������, ������� ����� ���������� ��� ������� �� �������
+	// устанавливаем функцию, которая будет вызываться при нажатии на клавишу
 	glutKeyboardFunc(keyboardFunc);
 
-	// �������� ���� ��������� ��������� ��
+	// основной цикл обработки сообщений ОС
 	glutMainLoop();
 };
